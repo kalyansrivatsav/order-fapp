cf78683ea37fcdae4bb4f5ec47a68b9fed33c071
rajusrivatsavlti <rajupalepu.srivatsav@lntinfotech.com>
2023-03-07 15:54:18 +0530

    function deploy

--- src/main/java/com/lti/OrderFunctionApp.java
+++ src/main/java/com/lti/OrderFunctionApp.java
@@ -1,7 +1,9 @@
 package com.lti;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class OrderFunctionApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderFunctionApp.class,args);
